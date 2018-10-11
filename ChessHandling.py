b97422a34404ba04823190b1c143573cdc9b7d99
jacob <jgoldverg@gmail.com>
2018-10-10 23:11:05 -0400

    added command line args its fully working now

--- ChessHandling.py
+++ ChessHandling.py
@@ -3,10 +3,6 @@ import os
 import fileinput
 import numpy as np
 
-filePathTest = '/shared/projects/regan/Chess/CSE712/MOVES/AA1025Kom10moves.txt'
-
-file_path_local = '/home/jacob/ChessIndependentStudy/MOVES/AA1025Kom10moves.txt'
-
 local_directory_test = '/home/jacob/ChessIndependentStudy/MOVES'
 # Looking up the points to convert them
 blackPointsDictionary = {'p': 1, 'r': 5, 'n': 3, 'b': 3, 'q': 9}
@@ -14,7 +10,6 @@ whitePointsDictionary = {'P': 1, 'R': 5, 'N': 3, 'B': 3, 'Q': 9}
 
 columns_list = ["FEN-Position", "#times", "Result", "WhiteR", "BlackR", "E", "EMove", "EMV", "MovePlayed",
                 "MovePlayedValue", "ValueDifference"]
-df = pd.read_csv(file_path_local, names=columns_list)
 
 
 # method takes in string, for convenience I decided to take the whole string and just the fen seperately(not
@@ -25,7 +20,7 @@ def parse_fen(row):
     #    positions_to_calculate = str(row).split('\n')
     print()
     if '#' in str(row):
-        return {'new game'}
+        return 000
     for c in fen_str_no_space:
         if str(c) in blackPointsDictionary.keys():
             black_points += blackPointsDictionary.get(c)
@@ -33,13 +28,14 @@ def parse_fen(row):
             white_points += whitePointsDictionary.get(c)
         else:
             continue
-    return white_points - black_points
+    x = white_points - black_points
+    return int(x)
 
 
 # white win is a 1 black win is a -1 and a draw is 1/2 or (.5)
 
 
-result_options = {'1/2': .5, '0-1': -1, '1-0': 1}
+result_options = {'1/2': 0.5, '0-1': -1.0, '1-0': 1.0}
 
 
 def parse_result(row):
@@ -47,23 +43,31 @@ def parse_result(row):
     if row in result_options:
         return result_options.get(row)
     else:
-        return None
+        return 0.0
 
 
 # the method using parseResult and parseFen to see if there was a comeback so to speak.
 def ten_point_lead_and_lost(row):
-    if row['Score'] > 900.0 and row['NoTie'] == -1.0:
+    if row['Score'] > 900 and row['NoTie'] == -1.0:
         return 'black flip'
-    elif row['Score'] < -900.0 and row['NoTie'] == 1.0:
+    elif row['Score'] < -900 and row['NoTie'] == 1.0:
         return 'white flip'
     else:
-        return None
-
-def write_file(name):
-    df.to_csv(name + 'DataFrame')
+        return ''
 
 
-def load_multiple_files(path):
+def ten_point_calculate(point, result):
+    point = parse_fen(point)
+    result = parse_result(result)
+    if point > 900 and result == -1.0:
+        return 'black'
+    elif point < -900 and result == 1.0:
+        return 'white'
+    else:
+        return ''
+
+
+def load_multiple_files(path, column_names):
     if path is None:
         file_list_names = os.listdir(local_directory_test)
     else:
@@ -75,7 +79,4 @@ def load_multiple_files(path):
         np_array.append(data_frame.as_matrix())
 
     combo_np = np.vstack(np_array)
-    return pd.DataFrame(combo_np)
-
-
-df['Points'] = df['FEN-Position'].apply(parse_fen)
+    return combo_np
--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 import argparse
 import pandas as pd
 import ChessHandling
+import os
+import numpy as np
 
 
 def main():
@@ -8,32 +10,35 @@ def main():
     columns_list = ["FEN-Position", "#times", "Result", "WhiteR", "BlackR", "E", "EMove", "EMV", "MovePlayed",
                     "MovePlayedValue", "ValueDifference"]
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input", help="increase output verbosity", type=str)
+    parser.add_argument("--input", help="increase output verbosity MOVES for now", type=str)
     parser.add_argument("--tenptlead",
                         help="calculate if player had 10pt lead and lost specify specify an int of any value to run this",
                         type=int)
-    parser.add_argument('--write', help='after everything write the dataFrame to file specified by name will write to local directory', type=str)
+    parser.add_argument('--write',
+                        help='after everything write the dataFrame to file specified by name will write to local directory',
+                        type=str)
     args = parser.parse_args()
     df = pd.DataFrame
 
-    if args.input is not None:
+    if args.input is not None:  # if input is none then really nothing to do
         temp = input('is this a directory(1) or a file(0)')
-        if temp == 0:
+        if temp == 0:  # file
             df = pd.read_csv(args.input, names=columns_list)
-        elif temp == 1:
-            df = ChessHandling.load_multiple_files(args.input)
+        elif temp == 1:  # directory
+            file_list_names = os.listdir(args.input)  # get specified directory
+            np_array = []
+            for file_ in file_list_names:
+                data_frame = pd.read_csv(args.input + '/' + file_,
+                                         names=columns_list)  # make dataframe per file then combine them using a numpy array
+                np_array.append(data_frame.as_matrix())  # attatch dataframe to numpy array
+
+            df = pd.DataFrame(np.vstack(np_array), columns=columns_list)  # set df = to total
+
     if args.tenptlead is not None:
-        df['Score'] = df['FEN-Position'].apply(ChessHandling.parse_fen)
-        df['NoTie'] = df['Result'].apply(ChessHandling.parse_result)
-        df['TurnAround'] = df.apply(ChessHandling.ten_point_lead_and_lost, axis=1)
-        print(df['Score'])
-        print(df['NoTie'])
-        print(df['TurnAround'])
+        df['TurnAround'] = df.apply(lambda row: ChessHandling.ten_point_calculate(row['FEN-Position'], row['Result']), axis=1)
 
     if args.write is not None:
         df.to_csv(args.write)
-
-
 
 
 if __name__ == "__main__":
