8e461128157d5956d7cea815b9b15c721a4881fe
jacob <jgoldverg@gmail.com>
2019-01-27 21:27:45 -0500

    major changes

--- AI_File.py
+++ AI_File.py
@@ -10,197 +10,211 @@ import random as rnd
 
 
 class CleanAndAnalyze(object):
-    columns_list = ["FEN-Position", "#times", "Result", "WhiteR", "BlackR", "E", "EMove", "EMV", "MovePlayed",
-                    "MovePlayedValue", "ValueDifference"]
+    columns_list = ["FEN-Position", "Result", "WhiteR", "BlackR", "EMV", "MovePlayed"]
     z_dictionary = {}
 
     def __init__(self, df):
         self.df = pd.DataFrame(df)  # need to create the dataframe to process before dont need to clean
-        self.elems_game_start = []
-        self.df_relevant = pd.DataFrame
+        self.elems_game_dict = {}
         self.rating_group_dict_to_z = {}
+        self.length_mate_frequencies = {4: 0, 5: 0, 7: 0, 9: 0, 11: 0, 15: 0}
 
     def clean(self):  # cleaning method, it takes away all of the line information and drops them
-        arr = np.array(self.df['FEN-Position'].tolist())
+        print('cleaning/preparing df: ')
+        self.df.dropna(inplace=True, axis=0)
         arr_games = []
         indexs = []
         game_count = 0
-        for idx, row in self.df.iterrows():
-            if str(row['FEN-Position']).__contains__('#'):
-                game_count += 1
-                indexs.append(idx)
-                self.elems_game_start.append({row['FEN-Position'], arr[idx + 1]})
-                arr_games.append(game_count)
-            else:
-                arr_games.append(game_count)
-
-        self.df['NewGame'] = np.array(arr_games)
-        self.drop_extra(indexs)
-        print('size of cleaned df is ' + str(self.df['FEN-Position'].size))
-        print(self.df.head())
-
-    def drop_extra(self, indexs_to_drop):
-        self.df.drop(self.df.index[indexs_to_drop], inplace=True, axis=0)
-        self.df.drop(['#times', 'E', 'EMove', 'MovePlayed', 'ValueDifference'], inplace=True, axis=1)
-        self.df.dropna(inplace=True)
-
-    def group_games(self):
-        new_game_col = self.df['NewGame'].values.tolist()
-        new_game_np = np.array(new_game_col)
-
-    def create_all_columns(self):
-        # slowest way to iterate over a dataframe but it makes sense currently
-        average_rating = []  # this is the average rating but skewed to be more of a group
         loc = []  # length of checkmate
         nine_pt_lead = []
         rating_group = []
+        loc_grouped = []
         for idx, row in self.df.iterrows():
-            if pd.isna(row['WhiteR']):
-                print('dropping this row')
-                print(row['FEN-Position'])
-                self.df.drop(row, inplace=True)
-            average_rating.append(self.average_rating(row['WhiteR'], row['BlackR']))
-            loc.append(self.y_length_of_checkmate(row))
-            nine_pt_lead.append(self.nine_enhanced(row))
-            rating_group.append(self.rating_group(row['WhiteR'], row['BlackR']))
+            if str(row['FEN-Position']).__contains__('#') or str(row['FEN-Position']).__contains__(';'):
+                game_count += 1
+                indexs.append(idx)  # store idx
+                print(str(row['FEN-Position']))
+                self.elems_game_dict[game_count] = str(row['FEN-Position'])
+                arr_games.append(game_count)
+                # this should only be checking the FEN-Position but a bug in file AA1650 stored the #/and ; in a different row segment had to do this long check
+            else:
+                loc.append(self.length_of_checkmate(row))
+                loc_grouped.append(int(self.loc_group_column(loc[-1])))
+                nine_pt_lead.append(self.nine_enhanced(row))
+                rating_group.append(self.rating_group(row['WhiteR'], row['BlackR']))
 
+            arr_games.append(game_count)
+        self.df['Game'] = arr_games #index of game in the file
+        self.df.drop(self.df.index[indexs], inplace=True)
         self.df['NinePtLead'] = nine_pt_lead
-        self.df['AverageRating'] = average_rating
         self.df['Rating_Group'] = rating_group
         self.df['length_of_checkmate'] = loc
+        self.df['loc_group'] = loc_grouped
+        print('\n' + 'size of cleaned df is ' + str(self.df['FEN-Position'].size))
+        print(self.df.head())
 
-    def average_rating(self, white, black):  # Average rating to 25
-        avg = (float(white) + float(black)) / 2
-        return int(round(avg / 25) * 25)
-
+    # This method is a function more or less, meant to be called from .apply but using a loop instead same with the few methods below that just create columns the extra bloat aint great
     def rating_group(self, white, black):
-        avg = float(white) + float(black)
-        avg = avg / 2
-        return int(round(avg) / 200) * 200
-
-    # creating two columns nineptlead column and the average rating column, taking the average of the row and rounding to the highest multiple of 25
-    # def nine_pt_lead_and_average_rating(self):
-    #     # calculate the average rating of every single line
-    #     self.df['AverageRating'] = self.df.apply(lambda row: self.average_rating(row['WhiteR'], row['BlackR']), axis=1)
-    #     # apply calc_nine_pt_lead to every single row of the dataframe
-    #     self.df['NinePtLead'] = self.df.apply(lambda row: self.nine_pt_lead(row), axis=1)
-    #     # calculate the length of checkmate every single row of the dataframe
-    #     self.df['length_of_checkmate'] = self.df.apply(lambda row: self.y_length_of_checkmate(row), axis=1)
-    #     print(self.df.head())  # print the head everytime so i know it hasnt frozen
+        avg = (float(white) + float(black))/2
+        return round(avg / 25) * 25
 
     # takes in move value played engine move value and result
     # EMV -- engine move value
     # result column 0-1, 1-0, 1/2
-    def nine_pt_lead(self, row):
-        EMV = int(row['EMV'])
-        result = str(row['Result'])
-        if (EMV >= 10000) or (EMV <= -10000):
-            if result == '1/2-1/2':
-                return 'mate'
-            elif result == '0-1' and EMV > 0:
-                return 'choked'
-            elif EMV < 0 and result == '1-0':
-                return 'choked'
-            else:
-                return 'mate'
-        elif (901 < EMV < 10000) or (-901 > EMV > -10000):
-            return 'weird'
-        else:
-            return 'useless'
-
     def nine_enhanced(self, row):
         EMV = int(row['EMV'])
         result = str(row['Result'])
         if EMV >= 90000 and result != '1-0' or EMV <= -90000 and result != '0-1':
-            return 'missedMate'
+            return 2
         elif EMV >= 900 and result != '1-0' or EMV <= -900 and result != '0-1':
-            return 'lostBigLead'
-        else:
-            return 'useless'
-
-    def y_length_of_checkmate(self, row):
-        EMV = int(row['EMV'])
-        if abs(EMV) >= 99000:
-            return 100000-abs(EMV)
+            return 1
         else:
             return 0
 
-    # grouping function for  class specific dataframe
-    def group_mates(self, target):  # the way i see it is every emv can be grouped together by a variance of 30
-        nine_group = self.df[str(target)].value_counts().to_dict()
-        return nine_group
+    # calculate the length of checkmate column for that row supplied
+    def length_of_checkmate(self, row):
+        EMV = int(row['EMV'])
+        if abs(EMV) >= 99000:
+            return 100000 - abs(EMV)
+        else:
+            return 0
+
+    # This calculates out loc_group column value and is my groups esentially for the real length_of_checkmate
+    def loc_group_column(self, loc):
+        if loc == 0:
+            return 0
+        elif 4 > loc > 0:
+            return 4
+        elif 6 > loc > 3:
+            return 5
+        elif 9 > loc > 5:
+            return 7
+        elif 11 > loc > 8:
+            return 9
+        elif 16 > loc > 10:
+            return 11
+        elif 20 > loc > 15:
+            return 15
+        else:
+            return 20
 
     # make a hist passing the method to a list
-    def make_hist(self):
+    def make_hist(self, column):
         # self.df.plot.hist() #local copy cant run on metallica
         print(self.df.head())
-        col_hist = input('Enter column to do histogram of: ')
-        plt.hist(self.df[col_hist])
-        plt.xlabel(input('x-label'))
-        plt.ylabel(input('Enter y label'))
-        plt.title(input('Enter Title Name for this Histogram!'))
-        self.save('histogram_nineptlead')
+        t = str(input('To look at MissedMate:2, lostBigLead:1, useless:0, or just press enter for full df'))
+        if t != '':
+            x = self.df[self.df['NinePtLead'] == int(t)]
+            plt.hist(x[column])
+        else:
+            plt.hist(self.df[column])
 
-    # This returns a list of relelvant rows to the current program so where ever there is a value
-    def create_relevant_list(self):
-        index_drop = self.df[self.df['length_of_checkmate'] == 0].index
-        self.df.drop(index_drop, inplace=True)
+        plt.xlabel(input('Enter x axis label:'))
+        plt.ylabel(input('Enter y axis label:'))
+        plt.title(input('Enter title for graph: '))
+        self.save()
 
-    #
-    def make_z(self):
-        temp = self.df[self.df['length_of_checkmate'] > 0]
-        x = temp['Rating_Group'].value_counts().to_dict()
-        y = temp['length_of_checkmate'].value_counts().to_dict()
-        z = temp[ temp['NinePtLead'] == 'choked'] #all positions where the player choked and had a length of checkmate greater than 0
-        print(pd.crosstab(temp['Rating_Group'].values, temp['length_of_checkmate'].values))
-        print(y)
-        print(temp.groupby(['Rating_Group', 'length_of_checkmate']))
+    def game_analysis(self):
+        data = self.df[self.df['NinePtLead'] == 'missedMate']
+        self.df.groupby('Game')
 
-    # single var regression of specified feature and target will get from user input
-    def linear_regression(self):
-        self.create_relevant_list()
-        self.make_z()
+    # NinePtLead code: 
+    # 0 == move in a game such that the move is not a big lead, OR it is a big lead and the player won.
+    # 1 == move in a game where player had a big lead (not forced mate) but did not win the game.
+    # 2 == move in a game where player had a forced checkmate but did not win the game.
+    # 
+    # This z rating that is working of y against z regression
+    # Returns a pair of NumPy arrays [bucket_ratings], [frequency of (1 or 2) relative to all moves].
+    def rating_frequency_calculate(self):
+        ninptlead_rating_group = self.df[['NinePtLead', 'Rating_Group']]
+
+        (denominator_keys, denominator_freq) = np.unique(ninptlead_rating_group['Rating_Group'], return_counts=True)
+        # returns pair of arrays; note that second array are absolute counts, not yet frequencies
+
+        numerator_keys, numerator_freq = np.unique(ninptlead_rating_group[ninptlead_rating_group['NinePtLead'] != 0]['Rating_Group'], return_counts=True)
+
+        denominator_dict = dict(zip(denominator_keys, denominator_freq))
+        numerator_dict = dict(zip(numerator_keys, numerator_freq))
+        for i in denominator_dict.keys():
+            if i not in numerator_dict.keys():
+                numerator_dict[i] = 0
+        numerator = np.array(np.fromiter(numerator_dict.values(), dtype=np.float64))
+        denominator = np.array(np.fromiter(denominator_dict.values(), dtype=np.float64))
+        print(numerator_dict)
+        print(denominator_dict)
+        z = numerator/denominator   # array op
+        print(z)
+        return [denominator_keys, z]
+
+    # This is the z rating for x against z
+    def length_of_checkmate_frequency(self):
+        data = self.df[self.df['NinePtLead'] == 2]
+        numerator_x, numerator_freq = np.unique(data['loc_group'], return_counts=True)
+        denominator_y, denominator_freq = np.unique(list(self.df['loc_group']), return_counts=True)
+        print(numerator_x, numerator_freq)
+        print(denominator_y, denominator_freq)
+        denominator_y = np.delete(denominator_y, [0])  # delete chomps off initial 0 for no-checkmate
+        denominator_freq = np.delete(denominator_freq, [0])
+        z = numerator_freq/denominator_freq
+        return [denominator_y, z]
+
+    def multi_reg(self):
+        z_loc = self.length_of_checkmate_frequency()
+        z_rating = self.rating_frequency_calculate()
+        lm = LinearRegression()
+        lm.fit(X=self.df[['Rating_Group', 'loc_group']], y= self.df['NinePtLead'])
+        #plt.scatter(X= z_loc[0], ))
+
+    # this is the first method to use linear regression, this one makes a graph frequency of length of checkmate
+    def linear_regression(self, t):
         print(self.df.head())
-#        lm = LinearRegression().fit(X = self.df[])
-
+        z = self.length_of_checkmate_frequency()
+        if int(t) == 1:
+            z = self.rating_frequency_calculate()
+        lm = LinearRegression()
+        x = np.array(z[0]).reshape(-1, 1)
+        y = np.array(z[1]).reshape(-1, 1)
+        print(x)
+        print(y)
+        lm.fit(X=x, y=y)
+        pred = lm.predict(X=np.array(z[0]).reshape(-1,1))
+        plt.scatter(z[0], z[1])
+        plt.plot(z[0], pred, color='green', linewidth='3')
+        plt.xlabel(input('xlabel'))
+        plt.ylabel(input('ylabel'))
+        plt.title(input('enter title for graph"'))
+        self.save()
         print('end of linear regression')
 
-    # multi var linear regression
-    def multi_linear_reg(self, feature, target):
-        X = self.df[feature]
-        y = self.df[target]
+
+    def multi_regression(self):
+        z_rating = self.rating_frequency_calculate()
+        z_rating = self.length_of_checkmate_frequency()
+        no_nine = self.df[self.df['NinePtLead' == 2]]['Rating_Group', 'loc_group', 'NinePtLead']
         lm = LinearRegression()
-        lm.fit(X, y)
-        predictions = lm.predict(y)
-        plt.scatter(X, predictions)
-        plt.title(feature + ' ' + target)
-        plt.xlabel(feature)
-        plt.ylabel(target)
-        name = input('file name: ')
-        self.save(name + 'multilinearreg')
+        lm.fit(X=np.unique(np.array(self.df[['Rating_Group', 'loc_group', 'NinePtLead']])), y=np.array(no_nine['NinePtLead']).reshape(-1,1))
+        lm.predict(np.unique(np.array(self.df[['Rating_Group', 'loc_group', 'NinePtLead']])))
 
-    # function needed for metallica
-    def save(self, name):
+    # save the current plot call after every time u plot to close and ping for filename
+    def save(self):
+        name = input('Enter name for file, sorry if this is the second time:')
         name = name.strip()
-        plt.savefig(name + '.png')
+        plt.savefig(name + '.png', bbox_inches="tight")
+        plt.close()
 
-    def get_train_test_inds(self, y, train_proportion=0.7):
-        '''Generates indices, making random stratified split into training set and testing sets
-        with proportions train_proportion and (1-train_proportion) of initial sample.
-        y is any iterable indicating classes of each observation in the sample.
-        Initial proportions of classes inside training and
-        testing sets are preserved (stratified sampling).
-        '''
-        y = np.array(y)
-        train_inds = np.zeros(len(y), dtype=bool)
-        test_inds = np.zeros(len(y), dtype=bool)
-        values = np.unique(y)
-        for value in values:
-            value_inds = np.nonzero(y == value)[0]
-            np.random.shuffle(value_inds)
-            n = int(train_proportion * len(value_inds))
+    # dumb ignore more of a reference
+    def other_z(self):
+        rows_lost = self.df[self.df['length_of_checkmate'] != 0]  # rows that dont have a checkmate at that position
+        missed_mates = rows_lost[rows_lost['NinePtLead'] == 2]  # get all missed checkmates
+        numerator = missed_mates[missed_mates['length_of_checkmate'] > 0]
+        ratings = np.array(numerator['Group_Rating'])
+        ratings = np.unique(ratings, return_index=True)
+        print(ratings)
+        denominator = self.df[self.df['length_of_checkmate'] > 0]  # get all mate value moves and group by the group_rating which returns a series of frequency
+        print(denominator)
+        z = numerator / denominator
+        return z
 
-            train_inds[value_inds[:n]] = True
-            test_inds[value_inds[n:]] = True
-
-        return train_inds, test_inds
+    def write_nineptlead_moves(self, name):
+        temp = self.df[self.df['NinePtLead'] == 2]
+        temp.to_csv(name, header=False)
--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ import AI_File
 import pgnParser
 import numpy as np
 import matplotlib
+
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 
@@ -23,16 +24,30 @@ def main():
     parser.add_argument("--graph", help="graph the current loaded dataframe", dest='graph', type=int)
     parser.add_argument('--write', help='specify path, it will be written as a csv', type=str, dest='write')
     parser.add_argument('--clean', help='clean file', type=int, dest='clean')
+    parser.add_argument('-help', help='Explain', type=int, dest='help')
+    parser.add_argument('--normal', help='file that was written after the nineptlead function', type=str, dest='normal')
+
     args = parser.parse_args()
     df_analyzeobj = None  # when working with a single file use this
     df_array_analyze = []  # list of ai objects when working with a directory
     df_array = []  # raw dataframe list when working with a directory
     dirorfile = input('Is this a directory or a file? 0 for file, 1 for Directory:')
 
+    if args.help is not None:
+        print(
+            'Hi, to start this program it recquires some files Main.py AI_File.py Game.py and pgnParser.py please have all of them in the same directory, also you probably want to run a directory of files, make'
+            'what I recommend is make a a directory and have those files in it then just specify Full Path to dir. \n the AI_File contains all of the functions concering the data this Main is just a runner more or less')
+        print('-------------------------------------------------------------------')
+        print(
+            'Example executions: \n 1) python3 Main.py --inputMOVES /your/dir/to/file.txt --nineptlead 1 --graph 1 --write /where/to/write/file.txt')
+        print('the --nineptlead 1 is to generate all of the columns for MOVES file')
+        print(
+            'can use --inputPGN /dirtofile/.txt but it is not fully working the file is being read correctly and parsed into useable Game objects but not included in the analyses at all currently because of its raw format')
+        print()
     if args.inputMOVES is not None:
         if int(dirorfile) == 0:  # file
-            df_analyzeobj = AI_File.CleanAndAnalyze(pd.read_csv(args.inputMOVES, names=columns_list))
-            print(df_analyzeobj.df['FEN-Position'].size)
+            df_analyzeobj = AI_File.CleanAndAnalyze(pd.read_csv(args.inputMOVES, names=columns_list, low_memory=False))
+            print(len(df_analyzeobj.df['FEN-Position'].values.tolist()))
             print('loaded file' + '_' + '/' + str(args.inputMOVES).split('/')[-1])
         else:  # directory
             file_list_names = os.listdir(args.inputMOVES)  # get specified directory
@@ -40,17 +55,22 @@ def main():
             print(len(file_list_names))  # the list of directories to double check
             for iter_file in file_list_names:  # iterate through all files inside of directory
                 print(iter_file + 'loaded', end=' ')
-                df_array.append(pd.read_csv(args.inputMOVES + '/' + iter_file, names=columns_list))
-            df = pd.concat(df_array)
-            df_analyzeobj = AI_File.CleanAndAnalyze(df)
+                df_array.append(pd.read_csv(args.inputMOVES + '/' + iter_file, names=columns_list, low_memory=False))
+            df_analyzeobj = AI_File.CleanAndAnalyze(pd.concat(df_array))
 
         df_analyzeobj.clean()
         print('done loading files')
         print('After loading file head')
 
+    if args.normal is not None:
+        print('reading normal file')
+        index_list = ['FEN-Position', 'Result', 'WhiteR', 'BlackR', 'EMV', 'MovePlayedValue', 'NewGame', 'NinePtLead',
+                    'Rating_Group', 'length_of_checkmate', 'loc_group']
+        df_analyzeobj = AI_File.CleanAndAnalyze(pd.read_csv(args.normal, names=index_list, low_memory=False))
+
     if args.nineptlead is not None:
         print('Nine Point Lead stage')
-#        df_analyzeobj.clean()
+        #df_analyzeobj.clean()
         df_analyzeobj.create_all_columns()
         print('finished nineptlead')
         print(df_analyzeobj.df.head())
@@ -66,30 +86,29 @@ def main():
                 '4: multi var regression, must specify multiple features as csv! and the target should also be in csv format')
             print('5 is to show frequency of a submitted column name case sensitive:')
             print('6 is to quit the graphing phase and exit the program')
-            visualize = input('1:describe, 2:histogram, 3:linear regression. 4:multi-var linear regression, 5 show frequency of entered column, and 6 is to break out of graph and exit')
+            visualize = input('1:describe, 2:histogram, 3:linear regression. 4:multi-var linear regression, and 6 is to break out of graph and exit \n')
             if int(visualize) == 1:
                 print(df_analyzeobj.df.describe())
             elif int(visualize) == 2:
-                df_analyzeobj.make_hist()
+                print(df_analyzeobj.df.head())
+                column = str(input('Enter column to do a histogram on'))
+                df_analyzeobj.make_hist(column)
             elif int(visualize) == 3:
-                df_analyzeobj.linear_regression()
+                t = input('Do regression on loc_group or do it or on the ratings of the dataframe: 1 for ratings 2 for loc: ')
+                df_analyzeobj.linear_regression(t)
             elif int(visualize) == 4:
-                X = input('enter feature to set as the X: ')
-                X = str(X).strip().split(',')
-                y = input('enter target to set as y: ')
-                y = str(y).strip().split(',')
-                df_analyzeobj.multi_linear_reg(list(X), y)
-
-            elif int(visualize) == 5:
-                target = input('Please enter a column to show frequencies of: ')
-                dict_nine = df_analyzeobj.group_mates(target)
-                print(dict_nine)
+                df_analyzeobj.multi_linear_reg()
             else:
                 break
 
     if args.write is not None:
         print('writing dataframe to file')
-        df_analyzeobj.df.to_csv(args.write)
+        res = input(
+            'write dataframe with only moves of missedMate value or entire dataframe: 1 for missedMate 2 for regular')
+        if int(res) == 1:
+            df_analyzeobj.write_nineptlead_moves(str(args.write))
+        else:
+            df_analyzeobj.df.to_csv(args.write, header=False)
 
     if args.inputPGN is not None:
         pgnObj = pgnParser.PGN_2_FEN(args.inputPGN)
